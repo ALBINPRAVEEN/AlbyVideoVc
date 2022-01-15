abd76259770793fd0fea0ff8ffe787362039dac4
ALBY <68139513+ALBINPRAVEEN@users.noreply.github.com>
2022-01-15 07:56:49 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -19,7 +19,7 @@ buttons = [
     [
         InlineKeyboardButton('⚡️Developer', url='https://albinpraveen.ml'),
         InlineKeyboardButton('🧩Owner', url='https://t.me/i_am_albin_praveen'),
-        InlineKeyboardButton('➕★ɴᴀɴᴜ ᴀᴅᴅ ᴄʜᴇꜱᴜᴋᴏ➕', url='https://t.me/{BOT_USERNAME}?startgroup=true'), 
+        InlineKeyboardButton('➕★ᴀᴅᴅ ᴛᴏ ɢʀᴏᴜᴩ➕', url='https://t.me/{BOT_USERNAME}?startgroup=true'), 
     ]
     ]   
     ]
